95aedf8a8338e67b660cac8220b5891d3503c12b
shubham <=>
2025-04-05 23:49:21 +0530

    reporting and all chalan operation working

--- KhodalKrupaERP/Core/AppDbContext.cs
+++ KhodalKrupaERP/Core/AppDbContext.cs
@@ -13,17 +13,6 @@ namespace KhodalKrupaERP.Core
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Challan> Challans{ get; set; }
         public DbSet<ChallanTransaction> ChallanTransactions{ get; set; }
-
-        // THIS IS WHERE YOU ADD THE CONFIGURATION
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<ChallanTransaction>()
-                .HasRequired(c => c.Challan)
-                .WithMany(p => p.ChallanTransactions)
-                .HasForeignKey(c => c.ChallanId)
-                .WillCascadeOnDelete(true); // ✅ Enables cascade delete
-
-            base.OnModelCreating(modelBuilder);
-        }
+        
     }
 }
